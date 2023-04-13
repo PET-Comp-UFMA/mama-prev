5eee020f19d5c5f87cade949b95d4316d92c22e2
WilliamVSMartins <williamvaltherprogramador@gmail.com>
2023-04-13 17:27:06 -0300

    feat: add Prevention screens

---
+++ src/pages/Prevention/Primary.js
@@ -0,0 +1,206 @@
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, Image, StatusBar, TouchableOpacity, Dimensions} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+export default ({navigation}) => {
+
+    const [shouldShow1, setShouldShow1] = useState(false);
+    const [shouldShow2, setShouldShow2] = useState(false);
+
+    return(
+        <View style={styles.container}>
+            <StatusBar hidden={true} />
+            <View style={styles.navigationBar}>
+                <TouchableOpacity onPress={() => navigation.goBack()}>
+                    <Image style={styles.backImg} source={require('../../../assets/back-icon.png')}/>
+                </TouchableOpacity>
+                <Text style={styles.textNavigation}> Prevenção Primária</Text>
+            </View>
+            <ScrollView style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>
+                        Relaciona-se com o controle dos fatores de risco modificáveis e incentivo a adoção de comportamentos protetores, visto que estes aspectos podem ajudar a reduzir cerca de 30% o risco de desenvolver a doença.
+                    </Text>
+                    <Text style={styles.text}>
+                        Para mulheres com alto risco de ter a doença pelos fatores genéticos e hereditários, existem também a mastectomia profilática com reconstrução mamária e a quimioprevenção.
+                    </Text>
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.titleText}>
+                        Fatores de risco modificáveis e comportamentos protetores
+                    </Text>
+                    <View style={styles.imgContainer}>
+                        <Image style={styles.img1} source={require('../../../assets/habitos.png')}/>
+                    </View>
+                    <Text style={styles.text}>
+                        - Controlar o peso (IMC menor que 30kg/m²) principalmente na pós-menopausa;
+                    </Text>
+                    <Text style={styles.text}>
+                        - Alimentar-se de forma balanceada e saudável (ingestão de frutas, legumes, azeite e alimentos risco em ômega 3);
+                    </Text>
+                    <View style={styles.textContainer}>
+                    {shouldShow1 ?
+                    (   
+                        <View>
+                            <Text style={styles.text}>
+                                Uma ingestão rica em alimentos de origem vegetal como frutas, legumes, verduras, cereais integrais, feijões e outras leguminosas, e pobre em alimentos ultraprocessados, como aqueles prontos para consumo ou prontos para aquecer e bebidas açucaradas, podem prevenir novos casos de câncer.
+                            </Text>
+                            <View style={styles.textContainer}>
+                                <TouchableOpacity style={styles.buttonRef} onPress={() => setShouldShow1(!shouldShow1)}>
+                                    <Text style={styles.textButton}>
+                                        Ocultar
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    ) : (
+                        <TouchableOpacity style={styles.buttonRef} onPress={() => setShouldShow1(!shouldShow1)}>
+                            <Text style={styles.textButton}>
+                                Saiba Mais
+                            </Text>
+                        </TouchableOpacity>)}
+                    </View>
+                    <Text style={styles.text}>
+                        - Praticar atividade física (de preferência movimentar-se todos os dias);
+                    </Text>
+                    <View style={styles.textContainer}>
+                    {shouldShow2 ?
+                    (   
+                        <View>
+                            <Text style={styles.text}>
+                                Realize atividades físicas como parte da rotina diária, começando por aquelas que lhe deem prazer, como caminhar, andar de bicicleta, dançar e nadar. Quanto mais se movimenta o corpo, maior a proteção contra o câncer. Caminhar ou ir de bicicleta para o trabalho, subir pelas escadas em vez de usar os elevadores, estabelecer momentos com a família e/ou amigos para atividades ao ar livre e/ou em praças públicas são algumas opções para aumentar a atividade física no dia a dia. Não há necessidade de serem aquelas modalidades sistematizadas ou que demandem a contratação de serviços como academias, que também podem ser opções. Existem recomendações que sugerem a realização de pelo menos 30 minutos de atividade física por dia, mas já há evidências de que mesmo quando realizada por menos tempo a atividade física traz benefícios para a prevenção de câncer e para a saúde. Assim, se movimente naquelas modalidades de atividade física que você gostar. A duração (tempo) torna-se mais um elemento, não o principal. É importante limitar hábitos sedentários como assistir à televisão, usar por muito tempo celular, tablet e computador ou jogar videogame.
+                            </Text>
+                            <View style={styles.textContainer}>
+                                <TouchableOpacity style={styles.buttonRef} onPress={() => setShouldShow2(!shouldShow2)}>
+                                    <Text style={styles.textButton}>
+                                        Ocultar
+                                    </Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+                    ) : (
+                        <TouchableOpacity style={styles.buttonRef} onPress={() => setShouldShow2(!shouldShow2)}>
+                            <Text style={styles.textButton}>
+                                Saiba Mais
+                            </Text>
+                        </TouchableOpacity>)}
+                    </View>
+                    <Text style={styles.text}>
+                        - Amamentar.
+                    </Text>
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.titleText}>
+                        Mastectomia profilática com reconstrução mamária para mulheres com alto risco de desenvolver o câncer de mama
+                    </Text>
+                    <View style={styles.imgContainerSquare}>
+                        <Image style={styles.img2} source={require('../../../assets/mastectomia2.png')}/>
+                    </View>
+                    <Text style={styles.text}>
+                        A redução absoluta do risco de câncer de mama varia de 2-7%, dependendo da idade de realização da cirurgia , geralmente até 40 anos.
+                    </Text>
+                    <Text style={styles.text}>
+                        A decisão na indicação da cirurgia profilática bilateral deve ser discutida com a paciente levando-se em consideração todos os riscos e benefícios envolvidos. 
+                    </Text>
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.titleText}>
+                        Quimioprevenção para mulheres com alto risco de desenvolver o câncer de mama
+                    </Text>
+                    <View style={styles.imgContainerSquare}>
+                        <Image style={styles.img3} source={require('../../../assets/quimioprevencao2.png')}/>
+                    </View>
+                    <Text style={styles.text}>
+                        De acordo com o Instituto Nacional do Câncer José Alencar Gomes da Silva (INCA), o SUS não utiliza medicamentos quimiopreventivos.
+                    </Text>
+                    <Text style={styles.text}>
+                        De acordo com a Sociedade Brasileira de Mastologia (SBM): Pacientes com risco de Gail ≥ 1.7, mais de 35 anos de idade, expectativa de vida superior a 10 anos ou com carcinoma lobular in situ devem usar um desses medicamentos por 5 anos:
+                    </Text>
+                </View>
+                <View style={styles.imgContainer}>
+                    <Image style={styles.img4} source={require('../../../assets/tabelaDoenca2.png')}/>
+                </View>
+                
+                
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "#ffffff"
+    },
+    firstText:{
+        marginTop: 40,
+        alignItems: "center",
+        marginHorizontal: 20
+    },
+    textContainer:{
+        alignItems: "center",
+        marginHorizontal: 20
+    },
+    text:{
+        fontSize:18,
+        textAlign:"center",
+        opacity: 0.7,
+        marginVertical: 10,
+    },
+    titleText:{
+        fontWeight: "bold",
+        fontSize:24,
+        textAlign: "center",
+        color: "#ee88b6",
+        marginTop:30,
+        marginBottom: 10
+    },
+    imgContainer:{
+        alignItems: "center",
+        marginVertical: 10
+    },
+    imgContainerSquare:{
+        alignItems: "center",
+    },
+    img1:{
+        width: 310,
+        height: 63
+    },
+    img2:{
+        width: Dimensions.get('window').width * 0.8,
+        height: Dimensions.get('window').width * 0.8
+    },
+    img3:{
+        width: Dimensions.get('window').width * 0.8,
+        height: Dimensions.get('window').width * 0.55
+    },
+    img4:{
+        width: Dimensions.get('window').width * 0.8,
+        height: Dimensions.get('window').width * 0.58
+    },
+    buttonRef:{
+        backgroundColor: "#ee88b6",
+        borderRadius: 5,
+        padding: 5,
+        marginBottom: 20
+    },
+    textButton:{
+        fontSize: 16,
+        textAlign: "center",
+        color: "#ffffff"
+    },
+    navigationBar:{
+        flexDirection: "row",
+        backgroundColor: "#ee88b6",
+        alignItems: "center",
+        height: 70,
+    },
+    textNavigation:{
+        fontSize: 24,
+        color: "#ffffff"
+    },
+    backImg:{
+        width: 40,
+        height: 40
+    }
+})
---
+++ src/pages/Prevention/Secondary/TrackingHighRisk.js
@@ -0,0 +1,177 @@
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, StatusBar, TouchableOpacity, Image, Modal} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+
+
+export default ({navigation}) => {
+        const [visible, setVisible] = useState(false);
+    return(
+        <View style={styles.container}>
+            <StatusBar hidden={true} />
+            <Modal
+            transparent={true}
+            visible = {visible}
+            >   
+                <View style={{flex: 1, backgroundColor:"rgba(0,0,0,0.5)", alignItems: "center", justifyContent: "center"}}>
+                    <View style={{position: "absolute",flex:1,backgroundColor: '#ffffff', alignItems: 'center', marginHorizontal: 30, marginTop: 10,borderRadius: 20, paddingTop: 60, paddingBottom: 20}}>
+                        <View style={styles.modalBar}>
+                            <View style={{flex: 1, justifyContent: "center"}}>
+                                <Text style={{textAlign: 'center', marginVertical: 10, fontSize: 20, color: "#ffffff", marginLeft:20, fontWeight: "bold"}}>Título da Página</Text>
+                            </View>
+                            <TouchableOpacity style={styles.closeButton} onPress = {() => {setVisible(false)}}>
+                                <Image style={styles.closeImg} source={require('../../../../assets/close-icon.png')}/>
+                            </TouchableOpacity>
+                        </View>
+                        <Text style={{textAlign: 'center', marginVertical: 10, marginHorizontal:20, fontSize: 16}}>
+                            Rastreamento para População de Alto Risco
+                        </Text>
+                        <TouchableOpacity style={styles.buttonClose2} onPress={() => {setVisible(false)}}>
+                            <Text style={styles.textButtonM}>
+                                Fechar
+                            </Text>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+            </Modal>
+
+            <View style={styles.navigationBar}>
+                <View style={{flexDirection: "row"}}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Image style={styles.navigationImg} source={require('../../../../assets/back-icon.png')}/>
+                    </TouchableOpacity>
+                    <Text style={styles.textNavigation}> Rastreamento Alto Risco ...</Text>
+                </View>
+                <TouchableOpacity onPress={() => {setVisible(true)}}>
+                    <Image style={styles.navigationImg} source={require('../../../../assets/info-icon.png')}/>
+                </TouchableOpacity>
+            </View>
+
+            <ScrollView style={styles.container}>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>
+                        As pacientes que apresentam risco aumentado para o surgimento do câncer de mama devem receber uma abordagem diferente para o diagnóstico precoce quando comparadas à população geral.
+                    </Text>
+                    <Text style={styles.text}>
+                        De acordo com a SBM, as seguintes condições estão associadas a um maior risco de desenvolvimento de câncer de mama e devem receber uma atenção diferenciada para o diagnóstico precoce: 
+                    </Text>
+                    <Text style={styles.titleText}>
+                        História prévia de câncer de mama
+                    </Text>
+                    <Text style={styles.text}>
+                        - Exame físico a cada seis meses nos primeiros 2 anos e depois anualmente;{"\n"}{"\n"}- Mamografia anual ou seis meses após o término do tratamento conservador (unilateral na mama operada);{"\n"}{"\n"}- Ultrassonografia das mamas deve ser utilizado se a mamografia for classe BIRADS 0;{"\n"}{"\n"}- Ressonância magnética das mamas na suspeita de recidiva após tratamento conservador ou esclarecer dúvidas após Mamografia ou Ultrassonografia das mamas.
+                    </Text>
+                    <Text style={styles.titleText}> 
+                        Pacientes com risco de Gail maior ou igual a 1.7% de desenvolver câncer de mama em 5 anos (a partir dos 35 anos de idade)
+                    </Text>
+                    <Text style={styles.text}>
+                        - Exame clínico a cada 6-12 meses (iniciar na idade em que o risco de Gail seja maior ou igual a 1.7%);{"\n"}{"\n"}- Mamografia anual.
+                    </Text>
+                    <Text style={styles.titleText}>
+                        Mulheres com risco maior que 20% de desenvolver câncer de mama ao longo da vida (“<Text style={styles.textIta}>lifetime risk</Text>”) com antecedentes de carcinoma lobular <Text style={styles.textIta}>in situ</Text> (CLIS) ou hiperplasia ductal atípica (HDA) ou hiperplasia lobular atípica (HLA)
+                    </Text>
+                    <Text style={styles.text}>
+                        - Exame clínico a cada 6-12 meses após o diagnóstico de CLIS, HDA ou HLA;{"\n"}{"\n"}- Mamografia anual, iniciar após o diagnóstico (não iniciar antes dos 30 anos de idade);{"\n"}{"\n"}- Se disponível considerar Ressonância Magnética das Mamas (não antes dos 25 anos de idade).
+                    </Text>
+                    <Text style={styles.titleText}>
+                        Mulheres com risco maior que 20% ao longo da vida (“<Text style={styles.textIta}>lifetime risk</Text>”) baseado em modelos que utilizam variáveis da história familiar (modelos de CLAUS, BOADICEA, BRCAPRO, Tyrer-Cuzik)
+                    </Text>
+                    <Text style={styles.text}>
+                        - Exame físico a cada 6-12 meses;{"\n"}{"\n"}- Se disponível geneticista, encaminhar para aconselhamento genético;{"\n"}{"\n"}- Mamografia anual, iniciar 10 anos antes da idade da paciente mais jovem que teve câncer na família (não antes dos 30 anos de idade);{"\n"}{"\n"}- Ressonância Magnética das Mamas anual, iniciar 10 anos antes da idade da paciente mais jovem que teve câncer na família (não antes dos 25 anos de idade).
+                    </Text>
+                    <Text style={styles.titleText}> 
+                        Mulheres que receberam previamente irradiação em manto no tórax com menos de 30 anos de idade
+                    </Text>
+                    <Text style={styles.text}>
+                        - Idade atual menor que 25 anos: exame físico anual (iniciar 8-10 anos após a irradiação); autoexame (como autoconhecimento e orientar para reportar alterações ao médico);{"\n"}{"\n"}- Idade atual maior ou igual a 25 anos: exame clínico a cada 6-12 meses (iniciar 8-10 anos após a irradiação); Mamografia anual (não antes dos 30 anos, iniciar 8-10 anos após a irradiação); Ressonância Magnética das Mamas anual (não antes dos 25 anos, iniciar 8-10 anos após a radioterapia). 
+                    </Text>
+                    <Text style={styles.titleText}>
+                        Pacientes com mutação conhecida que aumentam risco de câncer de mama ou com história familiar sugestiva
+                    </Text>
+                    <Text style={styles.text}>
+                        - Se disponível geneticista, encaminhar para aconselhamento genético;{"\n"}{"\n"}- Ressonância Magnética das Mamas iniciar aos 25 anos de idade, anualmente;{"\n"}{"\n"}- Mamografia anual a partir dos 30 anos de idade.
+                    </Text>
+                </View>
+            </ScrollView>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: "#ffffff"
+    },
+    textContainer:{
+        alignItems: "center",
+        marginHorizontal: 20
+    },
+    text:{
+        fontSize:18,
+        textAlign: "center",
+        opacity: 0.7,
+        marginVertical: 10
+    },
+    textIta:{
+        fontStyle:"italic",
+        fontSize:24,
+        textAlign: "center",
+        fontWeight: "bold",
+        color: "#ee88b6",
+    },
+    titleText:{
+        fontSize:24,
+        textAlign: "center",
+        fontWeight: "bold",
+        color: "#ee88b6",
+        marginTop:30,
+        marginBottom: 10
+    },
+    closeImg:{
+        width: 30,
+        height: 30
+    },
+    navigationImg:{
+        width: 40,
+        height: 40
+    },
+    buttonClose2:{
+        backgroundColor: "#ee88b6",
+        borderRadius: 5,
+        padding: 10,
+    },
+    textButtonM:{
+        fontSize: 16,
+        textAlign: "center",
+        color: "#ffffff"
+    },
+    modalBar:{
+        position: "absolute",
+        top: 0,
+        borderTopRightRadius: 20,
+        borderTopLeftRadius: 20,
+        width: "100%",
+        flexDirection: "row",
+        backgroundColor: "#ee88b6",
+        alignItems: "center",
+        height: 50,
+        marginBottom: 20,
+        justifyContent:"space-between"
+    },
+    closeButton:{
+        justifyContent: "center",
+        height: 30, 
+        width:30,
+        marginRight: 10
+    },
+    navigationBar:{
+        flexDirection: "row",
+        backgroundColor: "#ee88b6",
+        alignItems: "center",
+        height: 70,
+        justifyContent:"space-between"
+    },
+    textNavigation:{
+        fontSize: 24,
+        color: "#ffffff"
+    },
+})
